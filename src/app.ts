8be3f4000d6df9551ace488382695aecc38b95e6
codeCampWithSifat <sayedhossainsifat300@.com>
2023-06-15 02:32:52 +0600

    module 13 er 7 thakay 10 porjonto done

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import { UserRouter } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
 const app: Application = express();
 
 // use all the middleware
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Application Users Route
 app.use('/api/v1/users', UserRouter);
+
+// Academic Semesters Route
+app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 
 // app.get('/', (req: Request, res: Response, next: NextFunction) => {
 //   // throw new ApiError(400, 'Allah Is Almighty');
---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from 'mongoose';
+import {
+  AcademicSemesterModel,
+  IAcademicSemester,
+} from './academicSemester.interface';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles,
+} from './academicSemester.constant';
+
+// const Month = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'December',
+// ];
+
+export const academicSemesterSchema = new Schema<IAcademicSemester>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: academicSemesterTitles,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: academicSemesterCodes,
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: academicSemesterMonths,
+    },
+  },
+  { timestamps: true }
+);
+
+// Handling Same Year And Same Semester Issue
+// Data --> Check ? same year && same semester
+
+export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
+  'AcademicSemester',
+  academicSemesterSchema
+);
